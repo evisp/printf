c5ce298c4ad29d96976a39bb7a21a8000988d1c2
evisp <evisplaku@gmail.com>
2022-03-12 04:00:04 -0800

    Test

---
+++ main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int main()
+{
+  _printf("%d\n", 102);
+  _printf("%d\n", 3.14);
+  _printf("%c\n", 'c');
+  _printf("test\n");
+  return 0;
+}
---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+
+/**
+ * struct print - struct for conversion specifiers
+ * @print: struct print
+ * @p: print function specified
+ */
+typedef struct print
+{
+  char *print;
+  int (*p)();
+} print_t;
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+int p_char(va_list arg);
+int p_str(va_list arg);
+int p_dec(va_list arg);
+int p_int(va_list arg);
+
+#endif
