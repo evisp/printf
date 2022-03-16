6bc7126f399875c111e7eaa737c3d252d3db7950
evisp <evisplaku@gmail.com>
2022-03-15 21:50:01 -0700

    Adding a differnt main

--- _printf.c
+++ _printf.c
@@ -9,6 +9,53 @@
  */
 int _printf(const char *format, ...)
 {
+
+  va_list arguments;
+  const char *p;
+  int num = 0;
+
+  if (format == NULL)
+    return (-1);
+  
+  va_start(arguments, format);
+
+  for (p = format; *p; p++)
+    {
+      if (*p == '%' && *p + 1 == '%')
+	{
+	  _putchar(*p), num++;
+	  continue;
+	}
+      else if (*p == '%' && *p + 1 != '%')
+	{
+	  switch (*++p)
+	    {
+	    case 's':
+	      num += p_str(arguments);
+	      break;
+	    case 'c':
+	      num += p_char(arguments);
+	      break;
+	    case '%':
+	      _putchar('%'), num++;
+	      break;
+	    case '\0':
+	      return (-1);
+	    case 'i':
+	    case 'd':
+	      num += p_int(arguments);
+	      break;
+	    default:
+	      _putchar('%'), _putchar(*p), num += 2;
+	    }
+	}
+      else
+	_putchar(*p), num++;
+    }
+  va_end(arguments);
+  return (num);
+  
+  /**
   va_list arg;
   unsigned int i, j, flag;
   unsigned int len = 0;
@@ -62,4 +109,5 @@ int _printf(const char *format, ...)
   va_end(arg);
 
   return (len);
+  */
 }
