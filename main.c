7b22cca0ab44a743d46d0d42d82de87174bc6b69
evisp <evisplaku@gmail.com>
2022-03-15 21:55:31 -0700

    Edit typos

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
+#include <limits.h>
+#include <stdio.h>
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main()
 {
   _printf("%d\n", 102);
   _printf("%d\n", 3.14);
   _printf("%c\n", 'c');
-  _printf("test\n");
+  _printf("%s\n", "test");
   return 0;
 }
